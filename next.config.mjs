a0755afb8928c53280c5f1266a0768e9c971edab
srlee <srlee@agilesoda.ai>
2025-04-17 04:42:34 +0000

    modify code todo: add smoketest (sse)

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,16 @@ import { setupDevPlatform } from "@cloudflare/next-on-pages/next-dev";
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
+  webpack: (config, { isServer, dev }) => {
+    if (dev && !isServer) {
+      config.watchOptions = {
+        poll: 1000, // 폴링 간격을 1초로 설정
+        aggregateTimeout: 300, // 변경 감지 후 다시 빌드하기 전 대기 시간
+        ignored: /node_modules/,
+      };
+    }
+    return config;
+  },
 };
 
 if (process.env.NODE_ENV === "development") {
